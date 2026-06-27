47f2efcd4f7ca83a61f45aa65156f2ca546b8418
jawad-niazi <jawadniaziniazi@gmail.com>
2026-06-27 22:15:37 +0500

    body and validation

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,24 @@
 import express from "express"
+import {body,validationResult} from "express-validator"
 
 const router=express.Router();
 
-router.post("/",(req,res)=>{
-    console.log(req.body)
-    res.send("user.js")
+const registerValidation=[
+    body('name').notEmpty().withMessage("please enter your name"),
+    body('email')
+    .isEmail().withMessage("please enter valid email")
+    .normalizeEmail(),
+    body("password")
+    .isLength({min:6}).withMessage("minimum length for password is 6")
+]
+
+router.post("/",registerValidation,(req,res)=>{
+    const err=validationResult(req);
+    if(!err.isEmpty()){
+       return res.status(400).json(err.array())
+    }
+    
+    res.send("user")
     console.log("user is runnng")
 })
 
